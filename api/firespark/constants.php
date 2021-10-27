8b8f3d922217e8f2823333fe11ecbe039f4fbeea
Sorin <dsorin95@gmail.com>
2021-10-27 15:28:30 +0000

    Added more constants.

--- api/firespark/constants.php
+++ api/firespark/constants.php
@@ -4,4 +4,19 @@
     define("DB_HOST", "localhost");
     define("DB_USER", "fsdbguy");
     define("DB_PASSWORD", "Kp6SS1W6imawhHKt");
+
+    define("KEY_EMAIL", "email");
+    define("KEY_PASSWORD", "password");
+    define("KEY_USERNAME", "username");
+    define("KEY_FIRSTLASTNAME", "firstlastname");
+
+    define("ERR_BAD_REQUEST", "Bad request.");
+    define("ERR_MISSING_REQUIRED_KEYS", "Missing required keys.");
+    define("ERR_CANNOT_USE_EMAIL", "Invalid email, or it is already in use.");
+    define("ERR_CANNOT_USE_PASSWORD", "Try a different password.");
+    define("ERR_CANNOT_USE_USERNAME", "Username is taken.");
+    define("ERR_CANNOT_USE_FIRSTLASTNAME", "Try a different first and last name.");
+    define("ERR_CREATE_USER", "Could not register user. Try again later.");
+
+    define("SUCCESS_CREATE_USER", "User has been successfully registered.");
 ?>
