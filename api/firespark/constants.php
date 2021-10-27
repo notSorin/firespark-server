1223bc1430a73199455584e5f2260e8d8286e557
Sorin <dsorin95@gmail.com>
2021-10-27 13:05:32 +0000

    Updated constants.

--- api/firespark/constants.php
+++ api/firespark/constants.php
@@ -1,4 +1,7 @@
 <?php
     define("DB_NAME", "firespark");
     define("DB_NAMEPRE", "firesparkpre");
+    define("DB_HOST", "localhost");
+    define("DB_USER", "fsdbguy");
+    define("DB_PASSWORD", "Kp6SS1W6imawhHKt");
 ?>
