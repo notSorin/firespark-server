a96c4bc4c80226606533c543cd7fb2f7bbdd0788
Sorin <dsorin95@gmail.com>
2021-10-27 22:05:17 +0000

    Createa a script to handle the creation of new sparks by users.

---
+++ api/pre/SendSpark.php
@@ -0,0 +1,64 @@
+<?php
+    require_once('../firespark/constants.php');
+    require_once('../firespark/Utils.php');
+
+    $response = array();
+
+    if(isPostRequest($_SERVER))
+    {
+        require_once('../firespark/SparksDAO.php');
+
+        $sparksDAO = new SparksDAO();
+        $headers = getallheaders();
+
+        if($sparksDAO->containsRequiredKeysAndHeaders($_POST, $headers))
+        {
+            $token = $headers[KEY_TOKEN_AUTH];
+            $tokenPayload = decodeToken($token);
+
+            if($tokenPayload != null)
+            {
+                $userid = $tokenPayload->userdata->userid;
+
+                //TODO: Before continuing here, check if the user is allowed to send a spark, because 
+                //they might have reached their limit of sparks per hour.
+
+                $sparkBody = $sparksDAO->preProcessSparkBody($_POST[KEY_SPARK_BODY]);
+
+                if($sparkBody != null)
+                {
+                    $spark = $sparksDAO->sendSpark($userid, $sparkBody);
+
+                    if($spark != null)
+                    {
+                        $response = createSuccessResponse($spark);
+                    }
+                    else
+                    {
+                        $response = createBadResponse(ERR_SEND_SPARK);
+                    }
+                }
+                else
+                {
+                    $response = createBadResponse(ERR_SEND_SPARK);
+                }
+            }
+            else
+            {
+                $response = createBadResponse(ERR_INVALID_CREDENTIALS);
+
+                $response[KEY_ERRNO] = ERRNO_INVALID_CREDENTIALS;
+            }
+        }
+        else
+        {
+            $response = createBadResponse(ERR_MISSING_REQUIRED_KEYS);
+        }
+    }
+    else
+    {
+        $response = createBadResponse(ERR_BAD_REQUEST);
+    }
+
+    echoAsJSON($response);
+?>
