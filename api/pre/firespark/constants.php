e64847819cfcd60ba40245e7c69ba91e7b66ce62
Sorin <dsorin95@gmail.com>
2021-11-05 21:02:24 +0000

    Added new constants. Organized all constants by categories.

--- api/pre/firespark/constants.php
+++ api/pre/firespark/constants.php
@@ -1,16 +1,20 @@
 <?php
+    //General constants.
     define("HS512", "HS512");
     define("TOKEN_ISSUER", "Firespark");
     define("TOKEN_AUDIENCE", "Firespark users");
     define("TOKEN_KEY", "Make*Sure-This)Is}A%VERY+Strong#Password!OR.bad.things/will:happen");
     define("THIRTY_DAYS", 2592000); //In seconds.
+    define("MAX_SPARK_BODY_LENGTH", 150);
 
+    //Database strings.
     define("DB_NAME", "firespark");
     define("DB_NAMEPRE", "firesparkpre");
     define("DB_HOST", "localhost");
     define("DB_USER", "fsdbguy");
     define("DB_PASSWORD", "Kp6SS1W6imawhHKt");
 
+    //Keys.
     define("KEY_EMAIL", "email");
     define("KEY_PASSWORD", "password");
     define("KEY_USERNAME", "username");
@@ -25,7 +29,19 @@
     define("KEY_ACTION_FOLLOW", "follow");
     define("KEY_ACTION_UNFOLLOW", "unfollow");
     define("KEY_SEARCH_NAME", "name");
+    define("KEY_SPARK_ID", "sparkid");
+    define("KEY_SPARK_BODY", "body");
+    define("KEY_SPARK_CREATED", "created");
+    define("KEY_TOKEN_AUTH", "Authorization");
+    define("KEY_TOKEN_USER_DATA", "userdata");
+    define("KEY_PROFILE", "profile");
+    define("KEY_PROFILE_SPARKS", "profile_sparks");
+    define("KEY_TOKEN", "token");
+    define("KEY_SPARK", "spark");
+    define("KEY_SPARK_COMMENTS", "spark_comments");
+    define("KEY_COMMENT_ID", "commentid");
 
+    //Error strings.
     define("ERR_BAD_REQUEST", "Bad request.");
     define("ERR_MISSING_REQUIRED_KEYS", "Missing required keys.");
     define("ERR_CANNOT_USE_EMAIL", "Invalid email, or it is already in use.");
@@ -34,7 +50,6 @@
     define("ERR_CANNOT_USE_FIRSTLASTNAME", "Try a different first and last name.");
     define("ERR_CREATE_USER", "Could not register user. Try again.");
     define("ERR_INVALID_CREDENTIALS", "Invalid credentials detected.");
-    define("ERRNO_INVALID_CREDENTIALS", "ERRNO_IC");
     define("ERR_SEND_SPARK", "Could not send spark. Try again.");
     define("ERR_DELETE_SPARK", "The Spark could not be deleted.");
     define("ERR_LIKE_SPARK", "The Spark could not be liked.");
@@ -46,26 +61,18 @@
     define("ERR_GET_PROFILE_DATA", "Could not retrieve profile data.");
     define("ERR_SEARCH_NAMES", "Could not search for profiles.");
     define("ERR_GET_SPARK_COMMENTS", "Could not retrieve spark comments.");
+    define("ERR_DELETE_COMMENT", "Could not delete comment.");
+    define("ERR_LOGIN_FAILURE", "Incorrect email or password.");
 
+    //Error codes.
+    define("ERRNO_INVALID_CREDENTIALS", "ERRNO_IC");
+
+    //Success strings.
     define("SUCCESS_CREATE_USER", "User has been successfully registered.");
     define("SUCCESS_DELETE_SPARK", "The Spark has been deleted.");
     define("SUCCESS_LIKE_SPARK", "The Spark has been liked.");
     define("SUCCESS_UNLIKE_SPARK", "The Spark has been unliked.");
     define("SUCCESS_FOLLOW_USER", "The User has been followed.");
     define("SUCCESS_UNFOLLOW_USER", "The User has been unfollowed.");
-
-    define("ERR_LOGIN_FAILURE", "Incorrect email or password.");
-
-    define("KEY_SPARK_ID", "sparkid");
-    define("KEY_SPARK_BODY", "body");
-    define("KEY_SPARK_CREATED", "created");
-    define("KEY_TOKEN_AUTH", "Authorization");
-    define("KEY_TOKEN_USER_DATA", "userdata");
-    define("KEY_PROFILE", "profile");
-    define("KEY_PROFILE_SPARKS", "profile_sparks");
-    define("KEY_TOKEN", "token");
-    define("KEY_SPARK", "spark");
-    define("KEY_SPARK_COMMENTS", "spark_comments");
-
-    define("MAX_SPARK_BODY_LENGTH", 150);
+    define("SUCCESS_DELETE_COMMENT", "The Comment has been deleted.");
 ?>
