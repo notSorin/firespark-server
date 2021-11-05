69e71791a5577dbee54b398194e30fe5ce123f3d
Sorin <dsorin95@gmail.com>
2021-11-05 18:46:39 +0000

    Created a class to hold comments' data.

---
+++ api/pre/firespark/Comment.php
@@ -0,0 +1,14 @@
+<?php
+    require_once('constants.php');
+
+    class Comment
+    {
+        public $commentid, $sparkid, $userid, $body, $created, $deleted,
+            $replytoid, $likes, $username, $firstlastname;
+
+        function __construct()
+        {
+
+        }
+    }
+?>
