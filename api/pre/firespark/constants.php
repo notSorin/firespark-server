b33a98c109b64b796691844b5a060ab0475fefdf
Sorin <dsorin95@gmail.com>
2021-12-18 12:29:17 +0000

    Added scripts for handling getting the following of a user.

--- api/pre/firespark/constants.php
+++ api/pre/firespark/constants.php
@@ -61,6 +61,7 @@
     define("ERR_GET_POPULAR_SPARKS", "Could not retrieve popular sparks.");
     define("ERR_GET_ALL_PROFILES", "Could not retrieve profiles.");
     define("ERR_GET_USER_FOLLOWERS", "Could not retrieve user followers.");
+    define("ERR_GET_USER_FOLLOWING", "Could not retrieve user following.");
 
     //Error codes.
     define("ERRNO_INVALID_CREDENTIALS", "ERRNO_IC");
